b161dd961f9a6d9ef4fbd519627634a6ae87c437
grosser <grosser.michael@gmail.com>
2013-09-17 09:22:10 -0700

    correct module order to avoid load errors

--- lib/open_id_authentication.rb
+++ lib/open_id_authentication.rb
@@ -3,51 +3,60 @@ require 'openid'
 require 'rack/openid'
 
 module OpenIdAuthentication
-  def self.new(app)
-    store = OpenIdAuthentication.store
-    if store.nil?
-      Rails.logger.warn "OpenIdAuthentication.store is nil. Using in-memory store."
+  module ControllerMethods
+    protected
+
+    # The parameter name of "openid_identifier" is used rather than
+    # the Rails convention "open_id_identifier" because that's what
+    # the specification dictates in order to get browser auto-complete
+    # working across sites
+    def using_open_id?(identifier = nil) #:doc:
+      identifier ||= open_id_identifier
+      !identifier.blank? || request.env[Rack::OpenID::RESPONSE]
     end
 
-    ::Rack::OpenID.new(app, OpenIdAuthentication.store)
-  end
+    def authenticate_with_open_id(identifier = nil, options = {}, &block) #:doc:
+      identifier ||= open_id_identifier
 
-  def self.store
-    @@store
-  end
-
-  def self.store=(*store_option)
-    store, *parameters = *([ store_option ].flatten)
-
-    @@store = case store
-    when :memory
-      require 'openid/store/memory'
-      OpenID::Store::Memory.new
-    when :file
-      require 'openid/store/filesystem'
-      OpenID::Store::Filesystem.new(Rails.root.join('tmp/openids'))
-    when :memcache
-      require 'memcache'
-      require 'openid/store/memcache'
-      OpenID::Store::Memcache.new(MemCache.new(parameters))
-    else
-      store
-    end
-  end
-
-  self.store = nil
-
-  if defined?(::Rails::Railtie)
-    class Railtie < ::Rails::Railtie
-      config.app_middleware.use OpenIdAuthentication
-
-      config.after_initialize do
-        OpenID::Util.logger = Rails.logger
+      if request.env[Rack::OpenID::RESPONSE]
+        complete_open_id_authentication(&block)
+      else
+        begin_open_id_authentication(identifier, options, &block)
       end
+    end
 
-      ActiveSupport.on_load :action_controller do
-        ActionController::Base.send :include, ControllerMethods
-        #ActionController::Base.extend ControllerMethods
+    private
+
+    def open_id_identifier
+      params[:openid_identifier] || params[:openid_url]
+    end
+
+    def begin_open_id_authentication(identifier, options = {})
+      options[:identifier] = identifier
+      value = Rack::OpenID.build_header(options)
+      response.headers[Rack::OpenID::AUTHENTICATE_HEADER] = value
+      head :unauthorized
+    end
+
+    def complete_open_id_authentication
+      response   = request.env[Rack::OpenID::RESPONSE]
+      identifier = response.display_identifier
+
+      case response.status
+      when OpenID::Consumer::SUCCESS
+        yield Result[:successful], identifier,
+          OpenID::SReg::Response.from_success_response(response),
+          OpenID::AX::FetchResponse.from_success_response(response)
+      when :missing
+        yield Result[:missing], identifier, nil
+      when :invalid
+        yield Result[:invalid], identifier, nil
+      when OpenID::Consumer::CANCEL
+        yield Result[:canceled], identifier, nil
+      when OpenID::Consumer::FAILURE
+        yield Result[:failed], identifier, nil
+      when OpenID::Consumer::SETUP_NEEDED
+        yield Result[:setup_needed], response.setup_url, nil
       end
     end
   end
@@ -88,59 +97,52 @@ module OpenIdAuthentication
     end
   end
 
-  module ControllerMethods
-    protected
-      # The parameter name of "openid_identifier" is used rather than
-      # the Rails convention "open_id_identifier" because that's what
-      # the specification dictates in order to get browser auto-complete
-      # working across sites
-      def using_open_id?(identifier = nil) #:doc:
-        identifier ||= open_id_identifier
-        !identifier.blank? || request.env[Rack::OpenID::RESPONSE]
+  # middleware creation
+  def self.new(app)
+    store = OpenIdAuthentication.store
+    if store.nil?
+      Rails.logger.warn "OpenIdAuthentication.store is nil. Using in-memory store."
+    end
+
+    ::Rack::OpenID.new(app, OpenIdAuthentication.store)
+  end
+
+  def self.store
+    @@store
+  end
+
+  def self.store=(*store_option)
+    store, *args = *([ store_option ].flatten)
+
+    @@store = case store
+    when :memory
+      require 'openid/store/memory'
+      OpenID::Store::Memory.new
+    when :file
+      require 'openid/store/filesystem'
+      OpenID::Store::Filesystem.new(Rails.root.join('tmp/openids'))
+    when :memcache
+      require 'memcache'
+      require 'openid/store/memcache'
+      OpenID::Store::Memcache.new(MemCache.new(args))
+    else
+      store
+    end
+  end
+
+  self.store = nil
+
+  if defined?(::Rails::Railtie)
+    class Railtie < ::Rails::Railtie
+      config.app_middleware.use OpenIdAuthentication
+
+      config.after_initialize do
+        OpenID::Util.logger = Rails.logger
       end
 
-      def authenticate_with_open_id(identifier = nil, options = {}, &block) #:doc:
-        identifier ||= open_id_identifier
-
-        if request.env[Rack::OpenID::RESPONSE]
-          complete_open_id_authentication(&block)
-        else
-          begin_open_id_authentication(identifier, options, &block)
-        end
+      ActiveSupport.on_load :action_controller do
+        ActionController::Base.send :include, ControllerMethods
       end
-
-    private
-      def open_id_identifier
-        params[:openid_identifier] || params[:openid_url]
-      end
-
-      def begin_open_id_authentication(identifier, options = {})
-        options[:identifier] = identifier
-        value = Rack::OpenID.build_header(options)
-        response.headers[Rack::OpenID::AUTHENTICATE_HEADER] = value
-        head :unauthorized
-      end
-
-      def complete_open_id_authentication
-        response   = request.env[Rack::OpenID::RESPONSE]
-        identifier = response.display_identifier
-
-        case response.status
-        when OpenID::Consumer::SUCCESS
-          yield Result[:successful], identifier,
-            OpenID::SReg::Response.from_success_response(response),
-            OpenID::AX::FetchResponse.from_success_response(response)
-        when :missing
-          yield Result[:missing], identifier, nil
-        when :invalid
-          yield Result[:invalid], identifier, nil
-        when OpenID::Consumer::CANCEL
-          yield Result[:canceled], identifier, nil
-        when OpenID::Consumer::FAILURE
-          yield Result[:failed], identifier, nil
-        when OpenID::Consumer::SETUP_NEEDED
-          yield Result[:setup_needed], response.setup_url, nil
-        end
-      end
+    end
   end
 end
--- spec/open_id_authentication_spec.rb
+++ spec/open_id_authentication_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe OpenIdAuthentication do
-  # TODO
+  before do
+    OpenIdAuthentication.store = :memory
+  end
+
+  describe ".new" do
+    it "creates a Rack::OpenID" do
+      OpenIdAuthentication.new({}).class.should == Rack::OpenID
+    end
+  end
 end
