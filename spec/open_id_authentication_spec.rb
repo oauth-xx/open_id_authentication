6955d8cc90b3461bddacd517b4d31635d14e1b4d
Peter Boling <peter.boling@gmail.com>
2024-09-05 03:27:44 -0600

    ✅ More tests

--- spec/open_id_authentication_spec.rb
+++ spec/open_id_authentication_spec.rb
@@ -1,5 +1,13 @@
 RSpec.describe OpenIdAuthentication do
-  before do
-    OpenIdAuthentication.store = :memory
+  describe "::store" do
+    subject(:store) { OpenIdAuthentication.store = :memory }
+
+    before do
+      OpenIdAuthentication.store = nil
+    end
+
+    it "can be set to :memory" do
+      block_is_expected.to change { OpenIdAuthentication.store.class }.from(NilClass).to(OpenID::Store::Memory)
+    end
   end
 end
