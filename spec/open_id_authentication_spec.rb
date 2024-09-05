874b505ca44d908bc87c05bf81f9e93653e9b434
Peter Boling <peter.boling@gmail.com>
2024-09-05 03:28:54 -0600

    🚨 Linting

--- spec/open_id_authentication_spec.rb
+++ spec/open_id_authentication_spec.rb
@@ -1,13 +1,13 @@
 RSpec.describe OpenIdAuthentication do
   describe "::store" do
-    subject(:store) { OpenIdAuthentication.store = :memory }
+    subject(:store) { described_class.store = :memory }
 
     before do
-      OpenIdAuthentication.store = nil
+      described_class.store = nil
     end
 
     it "can be set to :memory" do
-      block_is_expected.to change { OpenIdAuthentication.store.class }.from(NilClass).to(OpenID::Store::Memory)
+      block_is_expected.to change { described_class.store.class }.from(NilClass).to(OpenID::Store::Memory)
     end
   end
 end
